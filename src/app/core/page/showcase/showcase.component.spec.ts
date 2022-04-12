725129501d9d25dfa0a5e8ff9866f646d8431dc0
Patricia <patricia.desantiago@tecnoturis.es>
2022-04-12 13:47:01 +0200

    test: showcase component

--- src/app/core/page/showcase/showcase.component.spec.ts
+++ src/app/core/page/showcase/showcase.component.spec.ts
@@ -1,26 +1,42 @@
 import { HttpClientModule } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ItemsService } from '../../service/items/items.service';
 
 import { ShowcaseComponent } from './showcase.component';
 
 describe('ShowcaseComponent', () => {
   let component: ShowcaseComponent;
+  let itemsService: ItemsService;
   let fixture: ComponentFixture<ShowcaseComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ShowcaseComponent],
       imports: [HttpClientModule],
+      providers: [ItemsService],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ShowcaseComponent);
     component = fixture.componentInstance;
+    itemsService = TestBed.inject(ItemsService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get items', () => {
+    const itemsServiceSpy = spyOn(itemsService, 'getItems').and.callThrough();
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(itemsService.getItems).toHaveBeenCalled();
+    expect(itemsServiceSpy).toHaveBeenCalled();
+    expect(itemsServiceSpy.calls.any()).toBeTrue();
   });
 });
--- src/app/core/page/showcase/showcase.module.ts
+++ src/app/core/page/showcase/showcase.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { ShowcaseComponent } from './showcase.component';
 
 @NgModule({
   declarations: [ShowcaseComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   exports: [ShowcaseComponent],
 })
 export class ShowcaseModule {}

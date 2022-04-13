3bdbdb9d475add0ba3b00fddfe810e1d1bf5bed4
Patricia <patricia.desantiago@tecnoturis.es>
2022-04-13 16:43:11 +0200

    feat: add searchbar to showcase page

--- src/app/core/page/showcase/showcase.component.ts
+++ src/app/core/page/showcase/showcase.component.ts
@@ -8,11 +8,29 @@ import { ItemsService } from '../../service/items/items.service';
   styleUrls: ['./showcase.component.scss'],
 })
 export class ShowcaseComponent implements OnInit {
-  itemsList!: Item[];
+  itemsList: Item[] = [];
+
+  filteredItems: Item[] = [];
 
   constructor(private itemsSvc: ItemsService) {}
 
   ngOnInit(): void {
-    this.itemsSvc.getItems().subscribe((items: Items) => (this.itemsList = items.items));
+    this.itemsSvc.getItems().subscribe((items: Items) => {
+      this.itemsList = items.items;
+      this.filteredItems = items.items;
+    });
+  }
+
+  search(ev: { searchType: 'description' | 'title' | 'price' | 'email'; searchValue: string }) {
+    if (!ev.searchValue) {
+      this.filteredItems = this.itemsList;
+    } else {
+      this.filteredItems = [];
+      this.itemsList.forEach((item: Item) => {
+        if (item[ev.searchType].toLowerCase().includes(ev.searchValue.toLowerCase())) {
+          this.filteredItems.push(item);
+        }
+      });
+    }
   }
 }

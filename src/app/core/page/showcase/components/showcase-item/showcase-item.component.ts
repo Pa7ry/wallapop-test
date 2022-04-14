d6e87fa17eb8e2c7a5634849b5d9cb5f206ab83e
Patricia <patricia.desantiago@tecnoturis.es>
2022-04-14 16:18:22 +0200

    feat: save item as favorite

--- src/app/core/page/showcase/components/showcase-item/showcase-item.component.ts
+++ src/app/core/page/showcase/components/showcase-item/showcase-item.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Item } from 'src/app/core/models/items.interface';
+import { FavoriteService } from 'src/app/core/service/favorite/favorite.service';
 
 @Component({
   selector: 'app-showcase-item',
   templateUrl: './showcase-item.component.html',
   styleUrls: ['./showcase-item.component.scss'],
 })
-export class ShowcaseItemComponent implements OnInit {
+export class ShowcaseItemComponent {
   @Input() item!: Item;
 
-  constructor() {}
+  constructor(private favoriteSvc: FavoriteService) {}
 
-  ngOnInit(): void {}
+  setFav(item: Item) {
+    this.favoriteSvc.favoriteItems$.subscribe((favItems: Item[]) => {
+      if (!favItems.includes(item)) {
+        favItems.push(item);
+        this.favoriteSvc.updateFavoriteItems(favItems);
+      }
+    });
+  }
 }

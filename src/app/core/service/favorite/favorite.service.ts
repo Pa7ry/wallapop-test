2a34c9aa41f5c712089111cc4909bba0310127de
Patricia <patricia.desantiago@tecnoturis.es>
2022-04-14 14:19:28 +0200

    feat: favorite service

---
+++ src/app/core/service/favorite/favorite.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
+import { Item } from '../../models/items.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoriteService {
+  private _favoriteItems: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>([]);
+
+  get favoriteItems$(): Observable<Item[]> {
+    return this._favoriteItems.asObservable();
+  }
+
+  updateFavoriteItems(items: Item[]) {
+    this._favoriteItems.next(items);
+  }
+}

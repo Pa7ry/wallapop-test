d2aac68cc513c3d6ca9326d2950808447eafc10e
Patricia <patricia.desantiago@tecnoturis.es>
2022-04-14 17:20:17 +0200

    feat: fav searchbar

--- src/app/shared/components/favorites/favorites.module.ts
+++ src/app/shared/components/favorites/favorites.module.ts
@@ -9,9 +9,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { FavItemComponent } from './fav-item/fav-item.component';
+import { FavSearchbarComponent } from './fav-searchbar/fav-searchbar.component';
 
 @NgModule({
-  declarations: [FavoritesComponent, FavItemComponent],
+  declarations: [FavoritesComponent, FavItemComponent, FavSearchbarComponent],
   imports: [CommonModule, MatButtonModule, MatIconModule, MatCardModule],
   exports: [FavoritesComponent],
 })

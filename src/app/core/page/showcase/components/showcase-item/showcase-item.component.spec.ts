967a059ce2464ef7dfa9779318da64fb2bde2321
Patricia <patricia.desantiago@tecnoturis.es>
2022-04-15 05:54:00 +0200

    style: showcase-item and showcase

--- src/app/core/page/showcase/components/showcase-item/showcase-item.component.spec.ts
+++ src/app/core/page/showcase/components/showcase-item/showcase-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
